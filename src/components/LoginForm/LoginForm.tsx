c3f34c221c7dc802b92db26539a407c4329dfdd8
Franco Durand <francoduve.25@gmail.com>
2024-06-11 22:17:14 -0500

    update LoginForm styles and functionality

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,9 @@
-import { Button, Input } from "@chakra-ui/react"
-import "./LoginForm.css"
+import { Button, FormControl, FormErrorMessage, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import IUser from "../../interfaces/iuser";
 import { UserService } from "../../services/user-service";
+import "./LoginForm.css";
 
 const buttonStyles = {
     bg: "#18181c",
@@ -14,21 +16,41 @@ const buttonStyles = {
 }
 
 export function LoginForm() {
+    const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [touched, setTouched] = useState({
+        email: false,
+        password: false,
+    });
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const handleBlur = (field: string) => {
+        setTouched({
+            ...touched,
+            [field]: true,
+        });
+    };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         try {
             const user = { email, password };
-            const response = await UserService.login(user);
+            const response: IUser = await UserService.login(user);
 
-            if (response && response.id) {
-                sessionStorage.setItem('userId', response.id);
+            if (response && response._id) {
+                sessionStorage.setItem('userId', response._id);
             }
+
+            navigate("/");
         } catch (e: any) {
-            alert("error")
+            if (e.name === "LoginError") {
+                onOpen();
+            }
+            else {
+                alert("error")
+            }
         }
     }
 
@@ -37,23 +59,53 @@ export function LoginForm() {
             <form className="form" onSubmit={handleSubmit}>
                 <label className="form__title">Bienvenido</label>
                 <div className="form__inputs">
-                    <Input
-                        placeholder='Email'
-                        width='auto'
-                        type="email"
-                        id="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <Input
-                        placeholder='Contraseña'
-                        width='auto'
-                        type="password"
-                        id="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                    <FormControl isInvalid={touched.email && !email} className="form__control">
+                        <Input
+                            placeholder='Email'
+                            width='auto'
+                            type="email"
+                            id="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            onBlur={() => handleBlur("email")}
+                        />
+                        {touched.email &&
+                            !email &&
+                            <FormErrorMessage>El email es requerido.</FormErrorMessage>
+                        }
+                    </FormControl>
+                    <FormControl isInvalid={touched.password && !password} className="form__control">
+                        <Input
+                            placeholder='Contraseña'
+                            width='auto'
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            onBlur={() => handleBlur("password")}
+                        />
+                        {touched.password &&
+                            !password &&
+                            <FormErrorMessage>La contraseña es requerida.</FormErrorMessage>
+                        }
+                    </FormControl>
                 </div>
                 <Button {...buttonStyles} type="submit">Iniciar sesión</Button>
             </form>
-        </div>
+
+            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Error</ModalHeader>
+                    <ModalBody>
+                        <span>Credenciales incorrectas</span>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Cerrar
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </div >
     )
 }
