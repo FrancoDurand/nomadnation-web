2a4a7a58e209940981972ca5e4956f90c8a5c186
Franco Durand <francoduve.25@gmail.com>
2024-06-12 19:12:36 -0500

    add Offer page and route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from './components/Header/Header'
 import { Home } from './pages/Home'
 import { LoginForm } from './components/LoginForm/LoginForm'
 import { RegisterForm } from './components/RegisterForm/RegisterForm'
+import { Offer } from './pages/Offer'
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
         <Route path='' element={<Home />} />
         <Route path='/login' element={<LoginForm />} />
         <Route path='/register' element={<RegisterForm />} />
+        <Route path='/offer/:id' element={<Offer />} />
       </Routes>
       <Footer />
     </>
