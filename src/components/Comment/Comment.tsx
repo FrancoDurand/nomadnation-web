36b3e93d1fc8f15a3733ba5d136a6ffedcf63b91
Franco Durand <francoduve.25@gmail.com>
2024-06-15 12:08:30 -0500

    add comment input

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,13 +1,56 @@
-import { Avatar } from "@chakra-ui/react"
+import { Avatar, Input, InputGroup, InputRightAddon } from "@chakra-ui/react"
+import { useState } from "react"
 import IComment from "../../interfaces/icomment"
+import IReviewPost from "../../interfaces/ireviewpost"
 import "./Comment.css"
+import { ReviewService } from "../../services/review-service"
 
-export function Comment({ commentsData }: { commentsData: IComment[] }) {
+const inputButtonStyles = {
+    cursor: "pointer",
+    borderColor: "#a7a8a9",
+    color: "#757575",
+    bg: "f3f4f6",
+    _hover: {
+        borderColor: "#CBD5E0"
+    }
+}
+
+
+export function Comment({ reviewId, commentsData }: { reviewId: string, commentsData: IComment[] }) {
+    const [comments, setComments] = useState<IComment[]>(commentsData);
+    const [comment, setComment] = useState("");
+
+    const handleClick = async () => {
+        const newComment: IReviewPost = {
+            _id: reviewId,
+            comments: [
+                {
+                    author: sessionStorage.getItem('userId') as string,
+                    comment: comment
+                }
+            ]
+        }
+
+        await ReviewService.comment(newComment);
+
+        const newCommentData: IComment = {
+            author: {
+                _id: sessionStorage.getItem('userId') as string,
+                name: sessionStorage.getItem('userName') as string,
+                profilePic: sessionStorage.getItem('userProfilePic') as string,
+            },
+            comment: comment
+        }
+
+        setComments([...comments, newCommentData]);
+        setComment("")
+    }
+
     return (
         <div className="comment-section">
             <h3 className="comment-section__title">Comentarios</h3>
             {
-                commentsData.map(comment =>
+                comments.map(comment =>
                     <div key={comment._id} className="comment">
                         <Avatar src={comment.author.profilePic as string} />
                         <div className="comment__info">
@@ -17,6 +60,12 @@ export function Comment({ commentsData }: { commentsData: IComment[] }) {
                     </div>
                 )
             }
+            <InputGroup>
+                <Input onChange={(e) => setComment(e.target.value)} value={comment} placeholder='Agrega un comentario' borderColor="#a7a8a9" />
+                <InputRightAddon  {...inputButtonStyles} onClick={() => handleClick()}>
+                    Publicar
+                </InputRightAddon>
+            </InputGroup>
         </div>
     )
 }
