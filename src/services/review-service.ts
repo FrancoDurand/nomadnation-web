04a6dedf117e172abecde8aaf579bddd31186591
Franco Durand <francoduve.25@gmail.com>
2024-06-23 02:06:56 -0500

    add create method for creating reviews with media attachments

--- src/services/review-service.ts
+++ src/services/review-service.ts
@@ -27,6 +27,42 @@ export class ReviewService extends ApiService {
          }
      } */
 
+    static async create(review: any) {
+        const requestURL = ReviewService.URL + "/create";
+
+        try {
+            const formData = new FormData();
+
+            formData.append("offer", review.offer);
+            formData.append("author", review.author);
+            formData.append("title", review.title);
+            formData.append("post", review.post);
+            //formData.append("media", review.media);
+
+            for (let i = 0; i < review.media.length; i++) {
+                formData.append("media", review.media[i], review.media[i].name);
+            }
+
+            const requestOptions: RequestInit = {
+                method: 'POST',
+                body: formData
+            };
+
+            const response = await fetch(requestURL, requestOptions);
+
+            if (!response.ok)
+                throw new Error('Invalid data');
+
+            const data = await response.json();
+
+            return data;
+        }
+        catch (e: any) {
+            console.error(e.message);
+            throw e;
+        }
+    }
+
     static async getByOffer(_id: string) {
         const requestURL = ReviewService.URL + "/getByOffer";
         const offerData = {
