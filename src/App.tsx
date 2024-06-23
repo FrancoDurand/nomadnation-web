6d6cab7d1f3587f3f513e3c3c7c254089aa1691e
Franco Durand <francoduve.25@gmail.com>
2024-06-23 17:08:43 -0500

    update import paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 //import "./App.css"
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/Home'
 import { LoginForm } from './components/LoginForm/LoginForm'
 import { RegisterForm } from './components/RegisterForm/RegisterForm'
 import { Offer } from './pages/Offer/Offer'
