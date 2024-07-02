a2c0a9188170775f7f108bf1bab1841ef57a3bb4
Franco Durand <francoduve.25@gmail.com>
2024-07-02 18:18:51 -0500

    api url update

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -1,3 +1,4 @@
 export class ApiService {
-    static URL: string = "https://nomadnation-api-gateway.onrender.com"
+    static URL: string = "https://nomadnation-api.onrender.com" //"http://localhost:3000"//"https://nomadnation-api-gateway.onrender.com" 
+    //52.186.84.97:3000
 }
